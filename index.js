f3ea4af71bac82754844dc62b7aebedabf597843
Jared Burnham <zlolris97@gmail.com>
2019-06-19 02:11:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ function add(a, b) {
   return a+b
 }
 
-function sub(a, b) {
-  return b-a
+function subtract(a, b) {
+  return a-b
 }
 
 function multiply(a, b) {
   return a*b
 }
 
-function devide(a, b) {
+function divide(a, b) {
   return a / b
 }
 
